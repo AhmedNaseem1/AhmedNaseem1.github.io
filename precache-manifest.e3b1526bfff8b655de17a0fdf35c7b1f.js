f758ccc60e40b3bc1a6f34e6b67b70569b45c8f1
AhmedNaseem1 <an1409647@gmail.com>
2020-07-10 20:21:34 +0200

    Updates

---
+++ precache-manifest.e3b1526bfff8b655de17a0fdf35c7b1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2e35f242dcfb78b74a8a2db1b96ba4",
+    "url": "/yasser/index.html"
+  },
+  {
+    "revision": "3cb1a828003c258afa97",
+    "url": "/yasser/static/css/2.fea45d51.chunk.css"
+  },
+  {
+    "revision": "32b2376d75b10684e9e1",
+    "url": "/yasser/static/css/main.6fc23e73.chunk.css"
+  },
+  {
+    "revision": "3cb1a828003c258afa97",
+    "url": "/yasser/static/js/2.ab943f0f.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/yasser/static/js/2.ab943f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b2376d75b10684e9e1",
+    "url": "/yasser/static/js/main.6630b49b.chunk.js"
+  },
+  {
+    "revision": "9a0eabe594600640892b",
+    "url": "/yasser/static/js/runtime-main.3bed1007.js"
+  },
+  {
+    "revision": "b62ecbfd04279dcc9a41fe1092166014",
+    "url": "/yasser/static/media/cairo.b62ecbfd.jpg"
+  },
+  {
+    "revision": "d1fad644d204ad654d65eca0cf16dcd2",
+    "url": "/yasser/static/media/images (1).d1fad644.jpg"
+  },
+  {
+    "revision": "b9c3d188d34ea71f3779172ec74a63eb",
+    "url": "/yasser/static/media/logo.b9c3d188.png"
+  },
+  {
+    "revision": "61ee5be15f7a8e7794f5d0e06335c925",
+    "url": "/yasser/static/media/photos_صورة_عالية_الوضوح_للكعبة_20170830261656.61ee5be1.jpg"
+  },
+  {
+    "revision": "c185ce1b75e5c63aac5b5bd5a17970d0",
+    "url": "/yasser/static/media/statue-of-liberty-1210001_1920.c185ce1b.jpg"
+  }
+]);
